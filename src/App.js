6ca3318e192eb394c21dc9a8dfc14329ef229f7e
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-01-30 10:52:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./navbar";
 
 function App() {
   return (
--- src/navbar.js
+++ src/navbar.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="navbar-container">
-                <div className="navbar-header">
-                    <div className="navbar-brand">Hallo Welt!</div>
-                        
-                    <div className="navbar-links">
-                        <div>Home</div>
-                        <div>Suche</div>
-                    </div>
-                </div>
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-header">
+          <div className="navbar-brand">Hallo Welt!</div>
 
-                <div className="navbar-actions">
-                    <button className="login-button">Login</button>
-                </div>
-            </div>
+          <div className="navbar-links">
+            <div>Home</div>
+            <div>Suche</div>
+          </div>
         </div>
-    );
-}
+
+        <div className="navbar-actions">
+          <button className="login-button">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
